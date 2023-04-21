8a702c4ddad4ad501aad03127edb6568d39af798
rodjus19@gmail.com <gusrod22>
2023-04-20 23:06:43 -0400

    updated functionality

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
+
+
 const emailInput = document.getElementById("email-input");
 const submitButton = document.getElementById("submit-button");
 const errorMessage = document.createElement("div");
-const arrowContainer = document.getElementById('arrow-container')
 errorMessage.classList.add("error-message");
 errorMessage.textContent = "Please enter a valid email address.";
-const emailIcon = document.getElementById("email-icon-container");
-
-
+const inputContainer = emailInput.parentNode;
+const emailIconContainer = document.querySelector('.error-icon-container');
+emailIconContainer.classList.add('hidden');
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
   const email = emailInput.value;
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const inputContainer = emailInput.parentNode;
+  const existingErrorMessage = inputContainer.nextElementSibling;
   if (!email) {
-    // Add this block to check if the input field is empty
     inputContainer.style.border = "1px solid red";
     errorMessage.textContent = "Please enter your email address.";
-    inputContainer.insertAdjacentElement("afterend", errorMessage);
-    emailIcon.classList.add("error-icon");
+    if(!existingErrorMessage || existingErrorMessage.className !== 'error-message') {
+      inputContainer.insertAdjacentElement("afterend", errorMessage);
+    }
+    emailIconContainer.classList.remove('hidden');
     return;
   } else if (!emailRegex.test(email)) {
-    // Add this block to check if the email address is formatted correctly
     inputContainer.style.border = "1px solid red";
     errorMessage.textContent = "Please enter a valid email address.";
-    inputContainer.insertAdjacentElement("afterend", errorMessage);
-    emailIcon.classList.add("error-icon");
+    if(!existingErrorMessage || existingErrorMessage.className !== 'error-message') {
+      inputContainer.insertAdjacentElement("afterend", errorMessage);
+    }
+    emailIconContainer.classList.remove('hidden');
     return;
   }
-  
-  errorMessage.remove();
-  emailIcon.classList.remove("error-icon");
-  inputContainer.style.border = 'none'; 
-  // Add your custom code here to submit the form or perform any other actions
+  if(existingErrorMessage && existingErrorMessage.className === 'error-message') {
+    existingErrorMessage.remove();
+  }
+  emailIconContainer.classList.add('hidden');
+  inputContainer.style.border = '1px solid #ccc'; 
 });
